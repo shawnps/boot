51271ead58cdcce593319fafd83d9984206747f9
Shawn Smith <shawnpsmith@gmail.com>
2013-11-19 22:35:39 +0900

    run the code on individual tabs

--- boot.js
+++ boot.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementsByTagName('head')[0].appendChild('<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">');
-  alert('loaded');
-}
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript({file: "inject.js"
+  });
+});
