0138d12272b1fc6220bd4c1b03ce09f3dc06bcec
Shawn Smith <shawnpsmith@gmail.com>
2013-11-19 21:13:59 +0900

    add manifest, initial code

---
+++ boot.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementsByTagName('head')[0].appendChild('<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">');
+  alert('loaded');
+}
