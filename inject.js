90eeed6574e3a231eae963aa50f7dcdfc08b2c94
Shawn Smith <shawnpsmith@gmail.com>
2013-11-21 20:24:27 +0900

    remove console.log

--- inject.js
+++ inject.js
@@ -3,7 +3,6 @@ link.href = '//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css';
 link.rel = 'stylesheet';
 d = document.createElement('div');
 d.setAttribute('class', 'container');
-console.log(d);
 document.getElementsByTagName('head')[0].appendChild(link);
 while (document.body.firstChild)
 {
